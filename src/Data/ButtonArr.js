f430ffd780bf6cbb3a4b65365097dcf7dde5869d
ssimilae <ssimilae@gmail.com>
2019-11-29 08:16:01 +0900

    Update ButtonArr.js

--- src/Data/ButtonArr.js
+++ src/Data/ButtonArr.js
@@ -1,11 +1,6 @@
 const buttonArr = [
-    { tabName: 'tabTopClothes', showName: 'Shirt', type: 'topclothes' },
-    { tabName: 'tabBotClothes', showName: 'Trouser', type: 'botclothes' },
-    { tabName: 'tabShoes', showName: 'Shoe', type: 'shoes' },
-    { tabName: 'tabHandBags', showName: 'Handbag', type: 'handbags' },
-    { tabName: 'tabNecklaces', showName: 'Necklace', type: 'necklaces' },
-    { tabName: 'tabHairStyle', showName: 'Hairstyle', type: 'hairstyle' },
-    { tabName: 'tabBackground', showName: 'Background', type: 'background' }
+    { tabName: 'tabTopClothes', showName: 'Color', type: 'topclothes' }, 
+    { tabName: 'tabNecklaces', showName: 'Pattern', type: 'necklaces' }, 
 ];
 
 export default buttonArr;
