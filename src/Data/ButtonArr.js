f3debea52725e0c2b415aa5c74f4f806043cb842
ssimilae <ssimilae@gmail.com>
2019-11-29 08:53:15 +0900

    Update ButtonArr.js

--- src/Data/ButtonArr.js
+++ src/Data/ButtonArr.js
@@ -1,5 +1,6 @@
 const buttonArr = [
     { tabName: 'tabTopClothes', showName: 'Color', type: 'topclothes' }, 
+      { tabName: 'tabBotClothes', showName: 'Line', type: 'botclothes' }, 
     { tabName: 'tabNecklaces', showName: 'Pattern', type: 'necklaces' }, 
 ];
 
